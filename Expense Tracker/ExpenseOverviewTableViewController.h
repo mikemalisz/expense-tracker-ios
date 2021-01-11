e1bb33aebc7d0d9897bc52d199a8076ed3f814ee
Mike Maliszewski <mikemalisz@gmail.com>
2021-01-10 20:30:18 -0600

    created expense overview table view controller files and added to storyboard, added mock data folder to gitignore

---
+++ Expense Tracker/ExpenseOverviewTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  ExpenseOverviewTableViewController.h
+//  Expense Tracker
+//
+//  Created by Mike Maliszewski on 1/10/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ExpenseOverviewTableViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
